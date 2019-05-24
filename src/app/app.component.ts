dc8538d3346fac29fae552396b4d72eb73e1ba3a
Joe Wheelock <joemwheelock@gmail.com>
2019-05-23 18:09:31 -0700

    clean slate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Hello World!';
-  age: number = 900000;
-  isShown: string = "I am being shown";
-  isNotShown: string = "I am not being shown";
-
-  person: object = {
-    name: "Joe",
-    age: 23,
-    course: "Angular"
-  };
-
-  people: object = [
-    {
-      name: "Joe",
-      age: 23,
-      course: "Angular"
-    },
-    {
-      name: "DJ",
-      age: 22,
-      course: "Angular"
-    },
-    {
-      name: "Jake",
-      age: 25,
-      course: "Angular"
-    },
-    {
-      name: "Gabe",
-      age: 25,
-      course: "Angular"
-    }
-  ];
-
-  colors: string[] = ['red', 'blue', 'green', 'yellow'];
-
-  myFunction(shouldShow: boolean): string {
-    return shouldShow ? this.isShown : this.isNotShown;
-  }
+  
 }
