e471fa41da6f8f0474615be83bfad6f206647581
Joe Wheelock <joemwheelock@gmail.com>
2019-05-30 20:47:41 -0700

    set up contact component

---
+++ src/app/contact/mock-contacts.ts
@@ -0,0 +1,6 @@
+import { ContactInterface } from '../interfaces/contact-interface';
+
+export const CONTACTS: ContactInterface[] = [
+    { id: 1, name: 'Bob', phone: '5551112233', email: 'bob@bob.bob', relation: 'Dealer'},
+    { id: 2, name: 'Smith', phone: '9283647174', email: 'smith@will.com', relation: 'Carlton'}
+];
