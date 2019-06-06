c421059f8859a0f7d077a68edbdc26e30de329a5
Joe Wheelock <joemwheelock@gmail.com>
2019-06-06 11:39:29 -0700

    finished first pipe (phone)

---
+++ src/app/components/phone.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'phone',
+    template: `<a href="tel:{{ phone }}">{{ phone | phone }}</a>`
+})
+export class PhoneComponent {
+    @Input() phone: string;
+}
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Contact } from '../models/contact';
+import { ContactInterface } from '../interfaces/contact-interface';
+import { CONTACTS } from '../contact/mock-contacts';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,16 @@ import { Injectable } from '@angular/core';
 export class ContactService {
 
   constructor() { }
+
+  newContact: ContactInterface = new Contact();
+
+  getContacts(): Observable<ContactInterface[]> {
+    return of(CONTACTS);
+  }
+
+  addNewContact(): ContactInterface {
+    const contactToAdd = this.newContact;
+    this.newContact = new Contact();
+    return contactToAdd;
+  }
 }
