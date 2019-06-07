f652d47563c6217dc64251622792ab710aab3c31
Joe Wheelock <joemwheelock@gmail.com>
2019-06-06 18:34:52 -0700

    generated amiibo component,interface,service

---
+++ src/app/amiibos/amiibos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amiibos',
+  templateUrl: './amiibos.component.html',
+  styleUrls: ['./amiibos.component.css']
+})
+export class AmiibosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
